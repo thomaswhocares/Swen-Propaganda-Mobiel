8744b7c17689e3a278a37e0e30de6ea139393118
Thomas <thomas.steffens@outlook.com>
2018-04-05 01:18:36 +0200

    test update

--- Python Test/Ampel.py
+++ Python Test/Ampel.py
@@ -2,46 +2,48 @@
 import RPi.GPIO as gpio
 import time
 
-#
-gpio.setmode(gpio.BOARD)
+#Alles was mit Pins zu tun hat sollte mit try umgeben werden
+try:
+    gpio.setmode(gpio.BOARD)
 
-#
-gpio.setup(29, gpio.OUT)
-gpio.setup(33, gpio.OUT)
-gpio.setup(37, gpio.OUT)
+    #
+    gpio.setup(29, gpio.OUT)
+    gpio.setup(33, gpio.OUT)
+    gpio.setup(37, gpio.OUT)
 
-#
-for i in range(2):
-    gpio.output(29, gpio.HIGH)
-    print("Pin 29 an")
-    time.sleep(1)
-    gpio.output(29, gpio.LOW)
-    print("Pin 29 aus")
+    #
+    for i in range(2):
+        gpio.output(29, gpio.HIGH)
+        print("Pin 29 an")
+        time.sleep(1)
+        gpio.output(29, gpio.LOW)
+        print("Pin 29 aus")
 
-    print("")
-    
-    
-    gpio.output(33, gpio.HIGH)
-    print("Pin 33 an")
-    time.sleep(1)
-    gpio.output(33, gpio.LOW)
-    print("Pin 33 aus")
+        print("")
+        
+        
+        gpio.output(33, gpio.HIGH)
+        print("Pin 33 an")
+        time.sleep(1)
+        gpio.output(33, gpio.LOW)
+        print("Pin 33 aus")
 
-    print("")
-    
+        print("")
+        
 
-    gpio.output(37, gpio.HIGH)
-    print("Pin 37 an")
-    time.sleep(1)
-    gpio.output(33, gpio.HIGH)
-    print("Pin 33 an")
-    time.sleep(1)
-    gpio.output(33, gpio.LOW)
-    print("Pin 33 aus")
-    gpio.output(37, gpio.LOW)
-    print("Pin 37 aus")
-    
-    print("")
+        gpio.output(37, gpio.HIGH)
+        print("Pin 37 an")
+        time.sleep(1)
+        gpio.output(33, gpio.HIGH)
+        print("Pin 33 an")
+        time.sleep(1)
+        gpio.output(33, gpio.LOW)
+        print("Pin 33 aus")
+        gpio.output(37, gpio.LOW)
+        print("Pin 37 aus")
+        
+        print("")
 
-#
-gpio.cleanup()
+except KeyboardInterrupt:
+    #GIPO pins werden wieder frei gegeben.
+    gpio.cleanup()
