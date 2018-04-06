e43f3389f64633e8563451f2e357f77f38c2ca70
thomaswhocares <38053170+thomaswhocares@users.noreply.github.com>
2018-04-06 11:40:05 +0000

    Doku wann gibt der sensor was aus

--- Python Test/sensorTest.py
+++ Python Test/sensorTest.py
@@ -8,6 +8,7 @@ try:
     GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     while True:
+        # wird eine Reflektion erkannt wird 0 ausgegeben. Wird nichts erkannt wird 1 ausgegeben.
         print(GPIO.input(16))
 
 except KeyboardInterrupt:
