b8fe6f98158ceeb422c9a4e1229e1a6802d23482
thomas.steffens <thomas.steffens@outlook.com>
2018-04-09 20:59:56 +0200

    update

--- App/app/src/main/java/com/example/thomas/myapplication/MainActivity.java
+++ App/app/src/main/java/com/example/thomas/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thomas.myapplication;
 
+import android.annotation.SuppressLint;
 import android.net.wifi.WifiInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -12,24 +13,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewConnectionStatus;
     private TextView localeIP;
     private EditText editTextIpAddress, editTextPort;
-    protected Button buttonConnect,buttonDisconnect, buttonControlLeft, buttonControlReverse, buttonControlRight;
+    private Button buttonConnect,buttonDisconnect, buttonControlLeftForwards, buttonControlLeftBackwards, buttonControlRightForwards,buttonControlRightBackwards;
     private Client client;
     private String serverIP;
     private int serverPort;
     private Handler handeler;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        localeIP = (TextView) findViewById(R.id.textEditLocalIP);
+        localeIP = findViewById(R.id.textEditLocalIP);
         localeIP.setText(localIpAddress());
 
         editTextIpAddress = findViewById(R.id.textEditServerIP);
@@ -37,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
         buttonConnect = findViewById(R.id.buttonServerConnect);
         buttonDisconnect = findViewById(R.id.buttonServerDisconnect);
         textViewConnectionStatus = findViewById(R.id.textViewConnectionStatus);
-        buttonControlLeft = findViewById(R.id.buttonControlLeft);
-        buttonControlReverse = findViewById(R.id.buttonControlReverse);
-        buttonControlRight = findViewById(R.id.buttonControlRight);
+
+        buttonControlLeftForwards = findViewById(R.id.buttonControlLeftForwards);
+        buttonControlLeftBackwards = findViewById(R.id.buttonControlLeftBackwards);
+        buttonControlRightForwards = findViewById(R.id.buttonControlRightForwards);
+        buttonControlRightBackwards = findViewById(R.id.buttonControlRightBackwards);
 
         editTextIpAddress.setText("192.168.2.171");
 
@@ -49,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
         buttonConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //Verhindert erneutes drücken des "Verbinden" buttons.
 
                 buttonConnect.setEnabled(false);
@@ -57,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 serverPort = Integer.parseInt(editTextPort.getText().toString());
 
                 //Instanzieren der Client Klasse
-                client = new Client(getApplicationContext(), handeler, buttonConnect, buttonDisconnect, buttonControlReverse, buttonControlLeft, buttonControlRight, textViewConnectionStatus);
+                client = new Client(getApplicationContext(), handeler, buttonConnect, buttonDisconnect, buttonControlLeftBackwards, buttonControlLeftForwards, buttonControlRightForwards, textViewConnectionStatus);
                 client.connectTo(serverIP, serverPort);
 
             }
@@ -69,16 +74,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonControlReverse.setOnTouchListener(new View.OnTouchListener() {
+        buttonControlLeftForwards.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    String command = "Rev down";
+                    String command = "LF down"; // Spin left wheel forwards
                     client.sendString(command);
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    String command = "Rev up";
+                    String command = "LF up";  //Stop
                     client.sendString(command);
                 }
                 return true;
@@ -86,37 +91,50 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        buttonControlLeft.setOnTouchListener(new View.OnTouchListener() {
-
+        buttonControlLeftBackwards.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    String command = "L down";
+                    String command = "LB down"; // Spin left wheel backwards
                     client.sendString(command);
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    String command = "L up";
+                    String command = "LB up";  // stop
                     client.sendString(command);
                 }
                 return true;
             }
-
         });
-        buttonControlRight.setOnTouchListener(new View.OnTouchListener() {
 
+
+        buttonControlRightForwards.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    String command = "R down";
+                    String command = "RF down"; // Spin right wheel backwards
                     client.sendString(command);
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    String command = "R up";
+                    String command = "RF up"; //Stop
                     client.sendString(command);
                 }
                 return true;
             }
+        });
 
+        buttonControlRightBackwards.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    String command = "RB down"; // Spin right wheel backwards
+                    client.sendString(command);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    String command = "RB up"; //Stop
+                    client.sendString(command);
+                }
+                return true;
+            }
         });
 
     }
@@ -125,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         WifiInfo wifiInf = wifiMan.getConnectionInfo();
         int ipAddress = wifiInf.getIpAddress();
         //copy pasta von stack overflow
-        String local_ip = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+        String local_ip = String.format(Locale.getDefault(),"%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
         return local_ip;
     }
 
