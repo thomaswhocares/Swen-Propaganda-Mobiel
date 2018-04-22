30b32123f4ce4251a3144a4155cb5a459e0127cf
thomas <thomas.steffens@outlook.com>
2018-04-22 11:32:03 +0200

    update verbindungen werden nicht mehr geschlossen.

--- Server/server.py
+++ Server/server.py
@@ -22,12 +22,17 @@ class Server:
             self.connected_instances=0
             self.interpreter = interpreter
 
-            print("Warte auf erste verbindung")
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.bind((server_host, server_port))
-            server_socket.listen(10)
-            
-            Thread.__init__(self)
+            try:
+                
+                print("Warte auf erste verbindung")
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_socket.bind((server_host, server_port))
+                server_socket.listen(10)
+                Thread.__init__(self)
+                
+            except:
+                print("Socket nicht frei")
+                self.interpreter.__end__
         
         def __end__(self):
             global keep_listening
@@ -35,33 +40,36 @@ class Server:
             server_socket.close()
                     
         def run(self):
+            self.commandThreads = []
             while keep_listening:
                 print("warte auf verbindung")
-                (clientsocket, address) = server_socket.accept()
+                (self.clientsocket, self.address) = server_socket.accept()
                 class Connection_recive_command_thread(Thread):
-                    def __init__(self,interpreter):
+                    def __init__(self,interpreter,clientsocket,address):
                         self.client_connected = True
                         self.interpreter = interpreter
+                        self.clientsocket = clientsocket
+                        self.address = address
                         Thread.__init__(self)
                     
                     def __end__(self):
                         self.client_connected = False
                         print("Löse Socket")
-                        clientsocket.detach()
+                        self.clientsocket.detach()
 
                     def run(self):
                         while self.client_connected:
-                            buf = clientsocket.recv(max_length)
+                            buf = self.clientsocket.recv(max_length)
                             if len(buf)<1:
                                 print("Client getrennt.")
                                 self.__end__() #client terminated connection
-                            print(buf[0])
-                            self.interpreter.execute_wheel_command(buf[0])
+                            else:
+                                self.interpreter.execute_wheel_command(buf[0])
                             
-                commandThreads = []
-                commandThreads.append(Connection_recive_command_thread(self.interpreter))
-                commandThreads[len(commandThreads)-1].start()
-                print("client nummer "+str(self.connected_instances)+" verbunden. platz nummer "+str(len(commandThreads)))
+                
+                self.commandThreads.append(Connection_recive_command_thread(self.interpreter,self.clientsocket,self.address))
+                self.commandThreads[len(self.commandThreads)-1].start()
+                print("client nummer "+str(self.connected_instances)+" verbunden. platz nummer "+str(len(self.commandThreads)))
                 self.connected_instances=self.connected_instances+1
                 
 
